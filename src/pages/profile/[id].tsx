353e981f46785cb0065f92aec21d5193bfbd2a97
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-01-22 14:22:40 +0530

    fix: user profile

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,17 +1,30 @@
 import { Container, Heading } from '@chakra-ui/react';
+import { userType } from 'interfaces/user';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import UserProfile from 'src/components/User/Profile/UserProfile';
 import { getUserByid } from 'src/lib/api/authHelper';
 const Profile = () => {
   const router = useRouter();
-
+  const [userProfile, setUserProfile] = useState<userType | null>();
+  const [loading, setLoading] = useState<boolean>(false);
   useEffect(() => {
     const fetchUser = async () => {
       if (router.query.id) {
+        setLoading(true);
         const data = await getUserByid(router.query.id as string);
-
-        console.log(data, 'data');
+        setUserProfile({
+          id: data.data.id,
+          name: data.data.name ?? '',
+          bio: data.data.bio,
+          contributions: data.data.Contribution ?? [],
+          picture: data.data.picture ?? '',
+          userName: data.data.username,
+          pub_key: data.data.publickey,
+          verified: data.data.verified,
+          projects: data.data.Projects ?? [],
+        });
+        setLoading(false);
       }
     };
     fetchUser();
@@ -24,7 +37,8 @@ const Profile = () => {
       >
         Profile
       </Heading>
-      <UserProfile />
+      {/* // loading state is need  here */}
+      {loading ? '' : <UserProfile user={userProfile as userType} />}
     </Container>
   );
 };
